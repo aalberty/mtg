8d3cef4b46163199b3973719e75ad2bea872db1e
aalberty <46965141+aalberty@users.noreply.github.com>
2023-05-04 17:16:36 +0000

    changes

--- ocr.py
+++ ocr.py
@@ -1,12 +1,33 @@
+import cv2
 import pytesseract
-from PIL import Image
 
 def ocr_core(filename):
-    """
-    This function will handle the core OCR processing of images.
-    """
-    # Open the image file using Pillow
-    with Image.open(filename) as img:
-        # Use pytesseract to convert the image into a string
-        text = pytesseract.image_to_string(img)
-        return text
+    # Load the image using OpenCV
+    img = cv2.imread(filename)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+    # Apply thresholding to create a binary image
+    binary = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY_INV | cv2.THRESH_OTSU)[1]
+
+    # Apply denoising to remove noise from the image
+    denoised = cv2.medianBlur(binary, 3)
+
+    # Apply deskewing to correct the orientation of the text
+    coords = cv2.findNonZero(denoised)
+    angle = cv2.minAreaRect(coords)[-1]
+    if angle < -45:
+        angle = -(90 + angle)
+    else:
+        angle = -angle
+    (h, w) = denoised.shape[:2]
+    center = (w // 2, h // 2)
+    M = cv2.getRotationMatrix2D(center, angle, 1.0)
+    rotated = cv2.warpAffine(denoised, M, (w, h), flags=cv2.INTER_CUBIC, borderMode=cv2.BORDER_REPLICATE)
+
+    # Perform OCR on the transformed image
+    text = pytesseract.image_to_string(rotated)
+
+    # Return the OCR result as a string
+    return text
